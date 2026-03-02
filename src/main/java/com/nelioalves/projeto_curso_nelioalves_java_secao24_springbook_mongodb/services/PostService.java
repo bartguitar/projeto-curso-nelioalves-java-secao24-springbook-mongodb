99e8faf54a5898240f5399277a473479a587066e
bartguitar <adrielbart@gmail.com>
2026-03-02 18:57:18 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/services/PostService.java
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.s
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,11 @@ public class PostService {
 
     public List<Post> findbyTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 
 }
