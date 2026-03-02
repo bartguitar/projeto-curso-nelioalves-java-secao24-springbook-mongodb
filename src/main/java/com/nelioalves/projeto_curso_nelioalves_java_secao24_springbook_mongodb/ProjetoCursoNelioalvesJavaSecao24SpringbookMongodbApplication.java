9462a4a5078da8f90efe3d0fdef2d552f93abc3b
bartguitar <adrielbart@gmail.com>
2026-03-02 10:21:09 -0300

    Project Created

---
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/ProjetoCursoNelioalvesJavaSecao24SpringbookMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCursoNelioalvesJavaSecao24SpringbookMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCursoNelioalvesJavaSecao24SpringbookMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/ProjetoCursoNelioalvesJavaSecao24SpringbookMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCursoNelioalvesJavaSecao24SpringbookMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
