a23174f3067cc2dacd7d310220f8001f4ef2ab5a
bartguitar <adrielbart@gmail.com>
2026-03-02 18:14:54 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/config/Instantiation.java
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain.Post;
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain.User;
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.dto.AuthorDTO;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.dto.CommentDTO;
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.repository.PostRepository;
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/domain/Post.java
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain;
 
 import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.dto.AuthorDTO;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
 
     public Post(){}
@@ -72,6 +77,14 @@ public class Post implements Serializable {
         this.author = author;
     }
 
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -83,4 +96,5 @@ public class Post implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
