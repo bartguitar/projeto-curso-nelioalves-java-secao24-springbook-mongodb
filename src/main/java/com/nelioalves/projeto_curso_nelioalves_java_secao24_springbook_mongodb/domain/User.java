b29a32a860ee2a0c17ff85d021a9abaed1ddfdb9
bartguitar <adrielbart@gmail.com>
2026-03-02 17:46:42 -0300

    Referenciando os posts do usuário

--- src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/domain/User.java
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/domain/User.java
@@ -1,9 +1,12 @@
 package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document(collection = "user")
@@ -16,8 +19,12 @@ public class User implements Serializable {
     private String name;
     private String email;
 
+    @DBRef(lazy = true)
+    private List<Post> posts =  new ArrayList<>();
+
 
     public User(){}
+
     public User(String id, String name, String email) {
         this.id = id;
         this.name = name;
@@ -49,6 +56,14 @@ public class User implements Serializable {
         this.email = email;
     }
 
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -60,4 +75,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
