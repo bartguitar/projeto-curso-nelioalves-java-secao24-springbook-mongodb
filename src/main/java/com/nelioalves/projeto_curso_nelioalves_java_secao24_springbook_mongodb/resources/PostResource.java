297424afebc912ebd00cfa817c6eba916e23eb70
bartguitar <adrielbart@gmail.com>
2026-03-02 18:02:29 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.resources;
+
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain.Post;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @RequestMapping(value="/{id}", method= RequestMethod.GET)
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+
+    }
+
+}
---
+++ src/main/java/com/nelioalves/projeto_curso_nelioalves_java_secao24_springbook_mongodb/services/PostService.java
@@ -0,0 +1,28 @@
+package com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.services;
+
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain.Post;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.domain.User;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.dto.UserDTO;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.repository.PostRepository;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.repository.UserRepository;
+import com.nelioalves.projeto_curso_nelioalves_java_secao24_springbook_mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
